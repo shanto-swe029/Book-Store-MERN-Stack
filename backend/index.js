678e67cec6196062bd5ca2f51a218165f30da8ad
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-03-26 00:05:13 +0600

    first commit: create first http route

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(request);
+}
+return res.status(234)).send("Welcome to MERN Stack tutorial!");
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port ${PORT}`);
+});
